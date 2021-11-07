6e8718a4b897ac90310e5165e3ab5524c46116dd
Mario Kukucov <sleepychild@abv.bg>
2021-11-07 19:02:57 +0200

    Create deploy-dashboard.sh

---
+++ docs/dashboard/deploy-dashboard.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install dashboard
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.4.0/aio/deploy/recommended.yaml
+
+# setup dashboard user
+kubectl apply -f dashboard-adminuser.yml
+kubectl apply -f cluster-role-binding.yml
+
+# get dashboard token
+kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
