1f4b815602959795621cc0d9bf3e512eb2dd61e6
Mario Kukucov <sleepychild@abv.bg>
2021-11-07 19:07:48 +0200

    Update deploy-dashboard.sh

--- docs/dashboard/deploy-dashboard.sh
+++ docs/dashboard/deploy-dashboard.sh
@@ -7,9 +7,5 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.4.0/a
 kubectl apply -f dashboard-adminuser.yml
 kubectl apply -f cluster-role-binding.yml
 
-# to enable dashboard access start kubectl proxy
-kubectl proxy
-
 # get dashboard token
 kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
-
